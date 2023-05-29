ef70cd58bc3a7b7c1a0430997a308e857f70782f
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2023-05-29 16:17:23 +0530

    Print message with first name and last name

--- src/main/java/com/example/greetingApp/controller/GreetingController.java
+++ src/main/java/com/example/greetingApp/controller/GreetingController.java
@@ -1,12 +1,13 @@
 package com.example.greetingApp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
     @GetMapping("/hello")
-    public String message(){
-        return "Hello World";
+    public String message(@RequestParam String firstName, @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " welcome to BridgeLabz";
     }
 }
